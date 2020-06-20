407b3dc9218551be76d1f82fd72ef775b43ef212
Lam Nguyen <lamcnguyen89@gmail.com>
2020-06-20 03:50:58 -0400

    Finished save button.

--- planner.js
+++ planner.js
@@ -3,6 +3,9 @@
     //Create an array that stores all the user's plans. This allows things to be stored and retrieved from local storage we create a new empty array with a length of 19 because there will be 19 hours total in this planner. At first this array will be empty but the user will add to it as he/she sees fit:
     userTasksArray = new Array(18)
 
+    //DOM element that links the Dynamically created time-blocks to html on the webpage and allows the time-blocks to become visible:
+    var $plannerElement =  
+
 // In this section of the code, we use moment.js to deal with the time elements on the web application.
 
     // Initiate variables and display the current time on the webpage:
@@ -12,7 +15,7 @@
         var displayMomentEl = document.getElementById("currentDay");
         // Display the code (time calculations) created by Moment.js onto the webpage:
         displayMomentEl.innerHTML = timeNow;
-        
+
         
 // In this section of the code, we create a loop that generates time blocks from 0600 to 2400. This loop will automatically display the hour for the particular time block.
 
@@ -61,7 +64,7 @@
                 let $hourlyTasks = $('<textarea>');
                 $hourlyTasks.attr('id', `input ${index}`);
                 $hourlyTasks.attr('hourIndex', index);
-                $hourlyTasks.attr('type', 'text');
+                $hourlyTasks.attr('type', 'text'); // *** Check this attribute to see if it makes sense***
 
                 // This code assigns the user tasks for that hour into the userTasksArray towards a particular index number. This allows data to be stored and retrieved from local storage in an orderly manner. The user tasks will be stored in order meaning that tasks scheduled for earlier in the day will be placed earlier in the index of the array:
                 $hourlyTasks.val(userTasksArray[index]);
@@ -69,18 +72,41 @@
                 // Finally, we insert the user input text box into the column and insert the column into the time-block row:
                 $rowHourly.append($colUserInput);
                 $colUserInput.append($hourlyTasks);
+
+        // After creating the user input box, we create the save button that stores the user tasks into local storage for later retrieval.
+
+                // First create the column that will house the save button. This column controls the width of the save button:
+                let $colSaveBtn = $('<div>');
+                $colSaveBtn.addClass('col-sm-1');
+
+                // Next we create the actual Save button.
+                let $saveBtn = $('<button>');
+                $saveBtn.addClass('saveBtn btn-primary'); // Check to see if there are any styling conflicts
+                $saveBtn.attr('id', `save ${index}`);
+                $saveBtn.attr('hourIndex', index);
+                $saveBtn.attr('type', 'button');
+                $saveBtn.attr('value', 'Save Tasks');
+
+                //Finally, we insert the Save button into the column and insert the column into the time block row:
+                $rowHourly.append($colSaveBtn);
+                $colSaveBtn.append($saveBtn);
+
+        // Next we implement the function that changes the color of the timeblock user input column (the 2nd column) based on the relative time in comparison to the current time. The current hour will be highlighted in red to show that time is almost up. The past hours will be grey. And future hours will be highlighted green:
                 
+                tasksUrgencyColor($rowHourly, timeHour);
+                
+        //Finally we have created the three columns in the time block: The time display column, the user input column and the column that houses the save button. Now we add it to the webpage by appending the row to an element on the html of the webpage. This the method that actually causes the webpage to show all the work we just did.
 
-
-        
-
-        
-        
-         
-        
-        
-        
+                
+ 
         
     }
+
+
+// Function to update row color:
+
+    function tasksUrgencyColor() {
+
+    };
 
         
